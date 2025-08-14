646d03754034c90652cedb19cf3c4cc4fc8e9071
Jame Zhan <jame@orderly.network>
2025-08-14 20:23:56 +0800

    remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  webpack: (config) => {
-    // fix Module not found: Can't resolve 'pino-pretty' warning https://github.com/pinojs/pino/issues/688
-    config.externals = [...config.externals, "pino-pretty"];
-    return config;
-  },
+  // webpack: (config) => {
+  //   // fix Module not found: Can't resolve 'pino-pretty' warning https://github.com/pinojs/pino/issues/688
+  //   config.externals = [...config.externals, "pino-pretty"];
+  //   return config;
+  // },
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
